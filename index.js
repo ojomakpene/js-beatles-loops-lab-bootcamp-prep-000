91875820d86f702b440d0a564fb9ac500cf05e76
Sani Abubakar <egbaojo@gmail.com>
2017-08-29 11:57:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,4 @@ function theBeatlesPlay() {
   }
   return j
 }
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive",  "He was never a vegetarian",  "He was a choir boy and boy scout",  "He hated the sound of his own voice"]
-var i = 0
- while (facts.length>i) {
-  console.log(facts[i] + '!!!')
-  i++
-}
-  return i
-}
+
